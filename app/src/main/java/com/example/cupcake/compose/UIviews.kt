b655b26dac8fe83bdbec8f6b6d40f2820641b443
Evtushenko Maksim <meevtushenko@sberbank.ru>
2024-04-21 20:02:07 +0300

    Часть 2 Создал кастомную вьюгруппу
     - Создать кастомную вьюгруппу, которая будет работать как Column, но располагать все элементы по диагонали

--- app/src/main/java/com/example/cupcake/compose/UIviews.kt
+++ app/src/main/java/com/example/cupcake/compose/UIviews.kt
@@ -3,6 +3,7 @@ package com.example.cupcake.compose
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -12,7 +13,12 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.Measurable
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
 
 /**
  * Утилитный класс со всеми вью
@@ -48,5 +54,55 @@ fun CupCakeAppBar(
             titleContentColor = MaterialTheme.colorScheme.onPrimary
         ),
         modifier = Modifier.fillMaxWidth()
+    )
+}
+
+@Composable
+fun DiagonalColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        content = content,
+        modifier = modifier
+    ) { measurables: List<Measurable>, constraints: Constraints ->
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints)
+        }
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            var x = 0
+            var y = 0
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = x, y = y)
+                x += placeable.width
+                y += placeable.height
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewDiagonalColumn() {
+    DiagonalColumn(
+        content = {
+            Text(text = "Text1", color = Color.White)
+
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onPrimary
+            )
+
+            Text(text = "Text2", color = Color.White)
+
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowForward,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onPrimary
+            )
+
+            Text(text = "Text3", color = Color.White)
+        }
     )
 }
