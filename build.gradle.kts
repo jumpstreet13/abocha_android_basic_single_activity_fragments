9d111ea65817770bbaf8964650afac61f4022e22
vrazdyakonov <razdyakonov.viktor@gmail.com>
2024-09-08 22:14:51 +0300

    Add plugin for metrics

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,27 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+}
+
+allprojects {
+    tasks.withType<KotlinCompilationTask<*>>().configureEach {
+        compilerOptions {
+            allWarningsAsErrors = false
+
+            freeCompilerArgs.addAll(
+                "-P",
+                "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" +
+                        layout.buildDirectory.asFile.get().absolutePath + "/compose_metrics"
+            )
+
+            freeCompilerArgs.addAll(
+                "-P",
+                "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" +
+                        layout.buildDirectory.asFile.get().absolutePath + "/compose_metrics"
+            )
+        }
+    }
 }
