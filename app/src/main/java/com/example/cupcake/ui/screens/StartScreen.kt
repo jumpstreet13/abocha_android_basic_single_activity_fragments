d74e2d4f2498a2a69d619c9ff68e95b0df347237
bmv-2143 <bmv-2143@yandex.ru>
2024-10-07 17:07:18 +0300

    feat: add scrollable surface to start screen

--- app/src/main/java/com/example/cupcake/ui/screens/StartScreen.kt
+++ app/src/main/java/com/example/cupcake/ui/screens/StartScreen.kt
@@ -6,8 +6,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,6 +19,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,7 +38,12 @@ fun StartScreen(sharedViewModel: OrderViewModel, onNavigateToFlavorScreen: () ->
         }
     )
     { paddingValues ->
-        StartScreenContent(sharedViewModel, onNavigateToFlavorScreen, modifier.padding(paddingValues))
+
+        Surface(modifier = modifier
+            .padding(paddingValues).padding(dimensionResource(id = R.dimen.side_margin))
+            .verticalScroll(rememberScrollState())) {
+            StartScreenContent(sharedViewModel, onNavigateToFlavorScreen, modifier)
+        }
     }
 }
 
